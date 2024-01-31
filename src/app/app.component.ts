b724e5e960a835e158efd8ac5dfaff7c7b787fea
Guido Maiola <maiolaguido@gmail.com>
2024-01-30 21:06:59 -0300

    Setting up audio play function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-02-piano';
+
+  aplicarSonido(note: number) : void {
+    const audio = new Audio();
+    audio.src = '../assets/sonidos/note' + note + '.wav';
+    audio.load();
+    audio.play();
+  }
 }
